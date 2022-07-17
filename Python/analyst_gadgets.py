0246540e97d79c3c6a4d87aa6a5bc77f0fb1e088
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-17 19:56:44 +0400

    Added function - row_header()
    row_header() - A little function replacing our data-frame's column-names by a preferred row's values, with the option to keep our dataframe from a "start" row.

--- Python/analyst_gadgets.py
+++ Python/analyst_gadgets.py
@@ -1,1 +1,19 @@
 # Handy analytical functions to be added for re-usbility in projects
+import pandas as pd
+
+
+# Setting the header (column-names) by a specific row
+def row_header(df, row=0, start=0):
+  # row = int   -> number/index of the row to be made a header
+  # start = int -> number/index of row from when the dataframe should "start"
+  
+  df.columns = df.iloc[row]
+  
+  if not start:
+    start += 1
+    
+  df = df[start:]
+  df.drop([0], inplace = True)
+  df.reset_index(inplace = True)
+  
+  return df
